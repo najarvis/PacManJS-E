a079a4131ab0c553497d2bfa5c07c3bba182a6b0
Nick Jarvis <najarvis2016@gmail.com>
2017-10-19 15:57:23 -0400

    Adds golang server executable in the event you don't have nodejs installed.

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "log"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    w.WriteHeader(http.StatusOK)
+    data, err := ioutil.ReadFile("pages/index.html");
+    if err != nil {
+        panic(err)
+    }
+
+    w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+    fmt.Fprint(w, string(data))
+
+}
+
+func main() {
+    http.HandleFunc("/", rootHandler)
+    http.Handle("/game_scripts/", http.StripPrefix("/game_scripts/", http.FileServer(http.Dir("game_scripts"))))
+    http.Handle("/res/", http.StripPrefix("/res/", http.FileServer(http.Dir("res"))))
+    fmt.Println("Running on localhost:8080");
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
